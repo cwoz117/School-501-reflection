969d74e73d9934f1396142790b96dbb4cf413685
cwoz117 <cwoz117@hotmail.ca>
2015-10-20 10:07:22 -0600

    Inspector grabs all objects, and starts output. Bugs: driver not calling it at the moment

--- Inspector.java
+++ Inspector.java
@@ -1,18 +1,12 @@
 
 import java.lang.reflect.*;
+import java.util.*;
 
 public class Inspector {
-	
-	private Method[] methods;
-	private Field[] fields;
-	private Class inspected;
-	private Class parent;
+	private List<Class<?>> storedObj;
 	
 	public Inspector(){
-		parent = null;
-		methods = null;
-		fields = null;
-		inspected = null;
+		storedObj = new ArrayList<Class<?>>();
 	}
 	
 	public void inspect(Object obj, boolean recursive){
@@ -21,41 +15,88 @@ public class Inspector {
 			throw new IllegalArgumentException("The object is null, nothing to inspect");
 		}
 		
-		// Get declaring class
-		inspected = obj.getClass();
+		storedObj.add(obj.getClass());
+		Field[] f = getAllDeclaredFields(obj);
+		for (int i = 0; i < f.length; i++) {
+			if ((recursive) && (!f[i].getType().isPrimitive()) && (!f[i].getType().isArray())){
+				if (storedObj.contains(f[i].getDeclaringClass())) {
+					storedObj.add(f[i].getType());
+				}
+			}
+		}
 		
-		// Name of immediate superclass
-		parent = inspected.getSuperclass();
-		// Name of interface(s)
+		for (Class<?> c: storedObj){
+			System.out.println(outString(c));
+		}
+	}
+	private String outString(Class<?> c){
+		String s = "";
+		Class[] exceptions;
+		Class[] parameters;
+		Method[] m = c.getDeclaredMethods();
+		int i, j;
 		
-		// Declared Methods, including
+		// Class header info
+		s += "Class: " + c.getName() + "\n";
+		s += "Super: " + c.getSuperclass() + "\n";
+		Class<?>[] iface = c.getInterfaces();
+		for (i = 0; i < iface.length; i++){
+			s += "Iface: " + iface[i].getName() + "\n";
+		}
 		
-			// Exceptions
-		
-			// parameters
-		
-			// return
-		
-			// modifiers
-		
-		// Constructors
-		
-			// Parameters
-		
-			// Modifiers
-		
-		// Fields
+		// Constructor info
+		Constructor<?>[] con = c.getConstructors();
+		for (i = 0; i < con.length; i++){
+			s += "Constructor: " + con[i].getName() + "\n";
 			
-			// Type
+			s += "\tParameters:\n";
+			parameters = con[i].getParameterTypes();
+			for(j = 0; j < parameters.length; j++){
+				s += "\t\t" + parameters[j].getName() + "\n";
+			}
+			
+			s += "\tModifiers\n";
+			s += "\t\t" + con[i].getModifiers() + "\n";
+		}
 		
-			// modifiers
+		// Field Info
 		
-		// Field values (pointer values in unsigned int's for objects if recursive is false.
 		
+		
+		// Method info
+		for (i = 0; i < m.length; i++){
+			s += "method: " + m[i].getName() + "\n";
+			
+			s += "\tExceptions:\n";
+			exceptions = m[i].getExceptionTypes();
+			for(j = 0; j < exceptions.length; j++){
+				s += "\t\t" + exceptions[j].getName() + "\n";
+			}
+			
+			s += "\tParameters:\n";
+			parameters = m[i].getParameterTypes();
+			for(j = 0; j < parameters.length; j++){
+				s += "\t\t" + parameters[j].getName() + "\n";
+			}
+			
+			s += "\tModifiers\n";
+			s += "\t\t" + m[i].getModifiers() + "\n";
+		}
+
+		return s;
 	}
 	
-	private void inspectHelper(){
-		
+	private Field[] getAllDeclaredFields(Object obj){
+		List<Field> ary = new ArrayList<Field>();
+		Class<?> cls = obj.getClass();
+		Field[] f;
+		while (cls != null){
+			f = cls.getDeclaredFields();
+			for (int i = 0; i < f.length; i++){
+				ary.add(f[i]);
+			}
+			cls = cls.getSuperclass();
+		}
+		return ((Field[]) ary.toArray());
 	}
-	
 }
