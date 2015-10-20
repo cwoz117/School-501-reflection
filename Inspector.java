b45628e18ac9c6b4a006e4bb33b3c0fe4219a9f7
Chris Wozniak <cwoz117@hotmail.ca>
2015-10-19 21:32:26 -0600

    started Inspector

--- Inspector.java
+++ Inspector.java
@@ -1,3 +1,61 @@
-public class Inspector{
 
+import java.lang.reflect.*;
+
+public class Inspector {
+	
+	private Method[] methods;
+	private Field[] fields;
+	private Class inspected;
+	private Class parent;
+	
+	public Inspector(){
+		parent = null;
+		methods = null;
+		fields = null;
+		inspected = null;
+	}
+	
+	public void inspect(Object obj, boolean recursive){
+
+		if (obj == null){
+			throw new IllegalArgumentException("The object is null, nothing to inspect");
+		}
+		
+		// Get declaring class
+		inspected = obj.getClass();
+		
+		// Name of immediate superclass
+		parent = inspected.getSuperclass();
+		// Name of interface(s)
+		
+		// Declared Methods, including
+		
+			// Exceptions
+		
+			// parameters
+		
+			// return
+		
+			// modifiers
+		
+		// Constructors
+		
+			// Parameters
+		
+			// Modifiers
+		
+		// Fields
+			
+			// Type
+		
+			// modifiers
+		
+		// Field values (pointer values in unsigned int's for objects if recursive is false.
+		
+	}
+	
+	private void inspectHelper(){
+		
+	}
+	
 }
